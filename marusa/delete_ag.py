05b9ed0ce09ceff4892363891671d003b50b24a2
marusazerjal <marusa.zerjal@anu.edu.au>
2019-09-06 15:12:26 +1000

    delete_ag

--- marusa/delete_ag.py
+++ marusa/delete_ag.py
@@ -10,14 +10,9 @@ import sys
 
 path = sys.argv[1]
 
-#~ files = []
-# r=root, d=directories, f = files
 for r, d, f in os.walk(path):
     for file in f:
         if 'T2m3ag' in file:
             filename = os.path.join(r, file)
-            #~ files.append()
             print filename
-            
-            
-            #~ os.remove("ChangedFile.csv")
+            os.remove(filename)
