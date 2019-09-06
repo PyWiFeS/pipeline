aea4281a5cf1801bcc914e76129fdc70bdadb3b8
marusazerjal <marusa.zerjal@anu.edu.au>
2019-09-06 18:26:50 +1000

    filename with obsdate and objectid

--- marusa/convert_fits_to_ascii.py
+++ marusa/convert_fits_to_ascii.py
@@ -10,7 +10,9 @@ import os
 # PyWiFeS
 import process_stellar as ps # WHEN running this without ssh -Y, I get RuntimeError: Invalid DISPLAY variable
 
-data_dir = os.path.join('/priv/mulga1/marusa/2m3reduced/', '%s/reduced_r_ys'%sys.argv[1]) # 20190304
+obsdate = sys.argv[1]
+
+data_dir = os.path.join('/priv/mulga1/marusa/2m3reduced/wifes/', '%s/reduced_r_ys'%sys.argv[1]) # 20190304
 print data_dir
 out_dir = os.path.join(data_dir, 'ascii')
 if not os.path.isdir(out_dir) and not os.path.exists(out_dir):
@@ -32,10 +34,15 @@ for path, subdirs, files in os.walk(data_dir):
             flux, wave = ps.read_and_find_star_p08(fl)
             spectrum, sig = ps.weighted_extract_spectrum(flux)
 
-            fln = name.replace('.fits', '.dat')
+            #~ fln = name.replace('.fits', '.dat')
+            
+            filename = '%s_%s.dat'%(obsdate, objectid)
+            
+            
             #~ #fln = fln.replace('.p08.', '.p08.%s.'%objectid)
             #~ #fln = fln.replace(' ', '_')
-            fln=os.path.join(out_dir, fln)
+            #~ fln=os.path.join(out_dir, fln)
+            fln=os.path.join(out_dir, filename)
             print fln
             print
             np.savetxt(fln, np.transpose([wave, spectrum]))
