d6e44533da3f798ef2f3163b33cadca48a16fb23
marusazerjal <marusa.zerjal@anu.edu.au>
2019-09-06 18:22:34 +1000

    convert fits to ascii

---
+++ marusa/convert_fits_to_ascii.py
@@ -0,0 +1,41 @@
+"""
+Convert all .p08.fits files to ascii spectra.
+"""
+
+import numpy as np
+from astropy.io import fits
+import sys
+import os
+
+# PyWiFeS
+import process_stellar as ps # WHEN running this without ssh -Y, I get RuntimeError: Invalid DISPLAY variable
+
+data_dir = os.path.join('/priv/mulga1/marusa/2m3reduced/', '%s/reduced_r_ys'%sys.argv[1]) # 20190304
+print data_dir
+out_dir = os.path.join(data_dir, 'ascii')
+if not os.path.isdir(out_dir) and not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+
+for path, subdirs, files in os.walk(data_dir):
+    for name in files:
+        fl=os.path.join(path, name)
+        if fl.endswith('.p08.fits'):
+            print fl
+            f=fits.open(fl)
+            header = f[0].header
+            objectid=header['OBJNAME']
+            run=header['RUN']
+            f.close()
+            
+            print run, objectid, fl
+
+            flux, wave = ps.read_and_find_star_p08(fl)
+            spectrum, sig = ps.weighted_extract_spectrum(flux)
+
+            fln = name.replace('.fits', '.dat')
+            #~ #fln = fln.replace('.p08.', '.p08.%s.'%objectid)
+            #~ #fln = fln.replace(' ', '_')
+            fln=os.path.join(out_dir, fln)
+            print fln
+            print
+            np.savetxt(fln, np.transpose([wave, spectrum]))
