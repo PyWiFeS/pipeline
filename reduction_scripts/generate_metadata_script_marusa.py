2607d99e8b7aa2b7f20a3d063b10f61036c56223
marusazerjal <marusa.zerjal@anu.edu.au>
2019-10-01 18:20:26 +1000

    keyword_indices

--- reduction_scripts/generate_metadata_script_marusa.py
+++ reduction_scripts/generate_metadata_script_marusa.py
@@ -628,6 +628,7 @@ def propose_missing_calib_files(mode=None, calstat=None):
     keywords_dark_zero = ['NAXIS1', 'NAXIS2', 'WINDOW', 'CCDSUM']
     keyword_indices = [keywords.index(x) for x in keywords_dark_zero]
     
+    print keyword_incides
     mode2 = mode[keyword_indices]
     print 'mode2', mode2
     
