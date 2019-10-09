9a10511fd290745df10f236903252250a16a12ab
marusazerjal <marusa.zerjal@anu.edu.au>
2019-10-09 12:10:59 +1100

    print

--- reduction_scripts/metadata_Red2Blue.py
+++ reduction_scripts/metadata_Red2Blue.py
@@ -54,7 +54,7 @@ for k, r in red.iteritems():
     
     if k!='sci' or k!='std':
         print 'kkkkk', k
-        print 'RRR', r
+        print 'RRR', r, type(r[0])
         print 'BBB', b
         run_r = set([x.split('-')[-1] for x in r])
         run_b = set([x.split('-')[-1] for x in b])
