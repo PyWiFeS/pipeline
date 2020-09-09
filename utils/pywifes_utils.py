519ec8eda936bef50ad33d1dafff29b8cbc81731
Adam Rains <adam.d.rains@gmail.com>
2020-09-09 14:46:14 +1000

    Updates to fits 1D extraction for stellar mode.

--- utils/pywifes_utils.py
+++ utils/pywifes_utils.py
@@ -75,7 +75,12 @@ def extract_stellar_spectra_ascii(root, night, steps = ["08", "09", "10"]):
                 np.savetxt(fln, np.transpose([wave, spectrum]))
 
 
-def extract_stellar_spectra_fits(root, night, steps = ["08", "09", "10"]):
+def extract_stellar_spectra_fits_all(
+    root, 
+    night, 
+    steps=["08", "09", "10"],
+    npix=7,
+    manual_click=False):
     """Make a fits data cube of the reduced and extracted stellar spectra, with
     a different HDU for each data reduction step. Store this new cube in a 
     folder called 'extracted_1D_cubes' in the night folder. By default:
@@ -92,18 +97,15 @@ def extract_stellar_spectra_fits(root, night, steps = ["08", "09", "10"]):
         The night to extract spectra from (in the form 201XYYZZ).
     steps: list of strings
         The PyWiFeS data reduction steps to extract and convert to 1D spectra.
+    npix: int
+        Number of pixels to extract for aperture photometry
+    manual_click: bool, default: True
+        Whether to ask for user input during extraction
     """
-    # Sort out directory structures
-    data_dir = os.path.join(root, night)
-    out_dir = os.path.join(data_dir, 'extracted_fits_1d')
-
-    if not os.path.isdir(out_dir) and not os.path.exists(out_dir):
-        os.mkdir(out_dir)
-
     # Get a list of the of the reduced files. Assume files have the naming
     # convention T2m3w[r/b]-20190828.104313-0031.p08.fits' where [r/b] will be
     # a single character indicating the arm of WiFeS used.
-    unique_obs = glob.glob(os.path.join(data_dir, "*", "*%s.fits" % steps[0]))
+    unique_obs = glob.glob(os.path.join(data_dir, "*%s.fits" % steps[0]))
 
     # Generalise by removing the suffixes
     unique_obs = [ob.replace(".p%s.fits" % steps[0], "") for ob in unique_obs]
@@ -112,45 +114,99 @@ def extract_stellar_spectra_fits(root, night, steps = ["08", "09", "10"]):
     # containing the original header information, and a HDU for each step of
     # the data reduction listed in steps.
     for ob in unique_obs:
-        # Get the list of fits files to extract 1D spectra from
-        fits_files = [ob + ".p%s.fits" % step for step in steps]
+        extract_stellar_spectra_fits(
+            ob,
+            root, 
+            night, 
+            steps=["08", "09", "10"],
+            npix=npix,
+            manual_click=manual_click)
 
-        # Get the header information from the first
-        header = fits.getheader(fits_files[0])
+def extract_stellar_spectra_fits(
+    ob,
+    root, 
+    night, 
+    steps=["08", "09", "10"],
+    npix=7,
+    manual_click=False,
+    out_dir=None,):
+    """Make a fits data cube of the reduced and extracted stellar spectra, with
+    a different HDU for each data reduction step. Store this new cube in a 
+    folder called 'extracted_1D_cubes' in the night folder. By default:
+     - 8: Final (non-fluxed or telluric corrected)
+     - 9: Final (fluxed)
+     -10: Final (fluxed and telluric corrected)
 
-        obj_id = header['OBJNAME'].replace(" ","")
+    Parameters:
+    -----------
+    ob: string
+        Unique part of the fits filename (i.e. excluding .pXX.fits)
+    root: string
+        The base path to the reduced data (i.e. where the nightly folders are
+        stored.)
+    night: string
+        The night to extract spectra from (in the form 201XYYZZ).
+    steps: list of strings
+        The PyWiFeS data reduction steps to extract and convert to 1D spectra.
+    npix: int
+        Number of pixels to extract for aperture photometry
+    manual_click: bool, default: True
+        Whether to ask for user input during extraction
+    out_dir: str or None, default None
+        Overriden output directory
+    """
+    # Sort out directory structures
+    data_dir = os.path.join(root, night, "reduced_*")
 
-        # Construct a new fits file
-        hdus = []
-        hdus.append(fits.PrimaryHDU(header=header))
+    if out_dir is None:
+        out_dir = os.path.join(data_dir, 'extracted_fits_1d')
+    
+    if not os.path.isdir(out_dir) and not os.path.exists(out_dir):
+        os.mkdir(out_dir)
 
-        print("Making cube for %s%s" % (ob, ".pXX.fits"))
+    # Get the list of fits files to extract 1D spectra from
+    fits_files = [ob + ".p%s.fits" % step for step in steps]
 
-        for fits_file, step in zip(fits_files, steps):
-            # Extract the 1D spectrum
-            print("\tExtracting 1D spectrum for step %s" % step)
-            flux, wave = ps.read_and_find_star_p08(fits_file)
-            spectrum, sig = ps.weighted_extract_spectrum(flux)
+    # Get the header information from the first
+    header = fits.getheader(fits_files[0])
 
-            # Make fits table from numpy record array
-            data = np.array([wave, spectrum, sig]).T.tolist()
-            rec = np.rec.array(data, names=["wave", "spectrum", "sigma"])
+    obj_id = header['OBJNAME'].replace(" ","")
 
-            hdus.append(fits.BinTableHDU.from_columns(rec))
-            hdus[-1].header["REDSTEP"] = step
-        
-        # Determine output format depending on spectral arm
-        if 'T2m3wr' in ob:
-            output_filename = '%s_%s_r.fits' % (night, obj_id)
-        elif 'T2m3wb' in ob:
-            output_filename = '%s_%s_b.fits' % (night, obj_id)
+    # Construct a new fits file
+    hdus = []
+    hdus.append(fits.PrimaryHDU(header=header))
 
-        output_filepath = os.path.join(out_dir, output_filename)
+    print("Making cube for %s%s" % (ob, ".pXX.fits"))
 
-        # Write the fits file
-        print("Writing cube to %s \n" % output_filename)
-        hdu_list = fits.HDUList(hdus)
-        hdu_list.writeto(output_filepath, overwrite=True)
+    for fits_file, step in zip(fits_files, steps):
+        # Extract the 1D spectrum
+        print("\tExtracting 1D spectrum for step %s" % step)
+        flux, wave = ps.read_and_find_star_p08(
+            fits_file,
+            npix=npix,
+            manual_click=manual_click)
+        spectrum, sig = ps.weighted_extract_spectrum(flux)
+
+        # Make fits table from numpy record array
+        data = np.array([wave, spectrum, sig]).T.tolist()
+        rec = np.rec.array(data, names=["wave", "spectrum", "sigma"])
+
+        hdus.append(fits.BinTableHDU.from_columns(rec))
+        hdus[-1].header["REDSTEP"] = step
+    
+    # Determine output format depending on spectral arm
+    if 'T2m3wr' in ob:
+        output_filename = '%s_%s_r.fits' % (night, obj_id)
+    elif 'T2m3wb' in ob:
+        output_filename = '%s_%s_b.fits' % (night, obj_id)
+
+    output_filepath = os.path.join(out_dir, output_filename)
+
+    # Write the fits file
+    print("Writing cube to %s \n" % output_filename)
+    hdu_list = fits.HDUList(hdus)
+    hdu_list.writeto(output_filepath, overwrite=True)
+
 
 def exclude_bad_frames(all_files, excluderun_filename):
     """
