e9e322649d2a349f61565521e4fd76af3c9f2d4d
Christopher Onken <christopher@onken.us>
2024-07-24 19:47:46 +1000

    Omit modules not yet committed

--- src/pywifes/__init__.py
+++ src/pywifes/__init__.py
@@ -2,4 +2,4 @@
 
 from . import logger_config, multiprocessing_utils, data_classifier, js_wifes_adr, \
     lacosmic, mpfit, optical_model, pywifes, wifes_adr, wifes_calib, wifes_ephemeris, \
-    wifes_imtrans, wifes_metadata, wifes_utils, wifes_wsol, wifes_2dim
+    wifes_imtrans, wifes_metadata, wifes_utils, wifes_wsol
