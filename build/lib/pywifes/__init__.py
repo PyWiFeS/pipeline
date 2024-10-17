200513c8d22ba6be3306b51f8cfcb784a1aafa40
Brent Miszalski <brent.miszalski@mq.edu.au>
2024-10-17 04:33:03 +0000

    Removed some unnecessary files.

--- build/lib/pywifes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Python package for optical data reduction pipeline."""
-
-from . import data_classifier, js_wifes_adr, lacosmic, \
-    logger_config, mpfit, multiprocessing_utils, optical_model, \
-    pywifes, quality_plots, wifes_adr, wifes_calib, wifes_ephemeris, \
-    wifes_imtrans, wifes_metadata, wifes_utils, wifes_wsol
