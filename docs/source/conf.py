caf5533dca01b3412747c6be069cfbac693c697d
Christopher Onken <christopher.onken@anu.edu.au>
2025-05-27 21:44:34 +1000

    conf update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,8 @@ extensions = ['sphinx.ext.autodoc',
 
 templates_path = ['_templates']
 exclude_patterns = []
-
+os.environ['PYWIFES_DIR'] = os.path.join(os.environ.get('READTHEDOCS_REPOSITORY_PATH'),
+                                         'reference_data')
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
